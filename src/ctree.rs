dfd1c51a8dee669649ea375ebfff6b94652dce0a
Thomas Harris <teh019283@gmail.com>
2016-12-22 12:24:42 -0500

    Forgot to commit for a while...

---
+++ src/ctree.rs
@@ -0,0 +1,51 @@
+use rand;
+use rand::Rng;
+
+pub struct CTree {
+    pub branches: Vec<Branch>,
+    pub max_width: usize,
+} 
+
+pub struct Branch {
+    pub width: usize,
+    pub ornaments: Vec<usize>,
+}
+
+
+impl CTree {
+    pub fn new(height: usize) -> CTree {
+        let h = if height > 4 { height } else { 4 };
+        let mut branches: Vec<Branch> = Vec::with_capacity(h);
+
+        // simple ornament distribution right now
+        for i in 0..height {
+            let w = (i*2)+1;
+
+            let mut rng = rand::thread_rng();
+            let mut ornaments = Vec::new();
+            let mut ready = 0;
+
+            for j in 1..w {
+                if ready == 0 && rng.gen() {
+                    ornaments.push(j);
+                    ready = 2;
+                } else if ready > 0 {
+                    ready -= 1;
+                }
+            }
+
+            let branch = Branch {
+                width: w,
+                ornaments: ornaments
+            };
+            
+            branches.push(branch);
+        }
+
+        let max_w = branches[branches.len()-1].width;
+        CTree {
+            branches: branches,
+            max_width: max_w,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate rand;
+extern crate term_painter;
+mod ctree;
+
+use ctree::CTree;
+use term_painter::ToStyle;
+use term_painter::Color::*;
+use term_painter::Attr::*;
+use rand;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let t = CTree::new(10);
+    print_ctree(t);
+}
+
+fn print_ctree(t: &CTree) {
+    let ocolors = vec![Red, Blue, Yellow];
+
+    for b in t.branches {
+        for i in 0..(b.width-t.max_width) {
+            print!(" ");
+        }
+
+        let mut os = b.ornaments.clone();
+        os.reverse();
+        let mut cur_ornament = os.pop();
+        for i in 0..width {
+            match (i, cur_ornament) {
+                (_, None) => print!("{}", Plain.bg(Green).paint(" "));
+                (i, Some(o)) where i == 0 => print
+            }
+        }
+    }
 }
