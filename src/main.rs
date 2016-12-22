ce159585454fc0318c7cac3b2e87a59b3165f1d3
Thomas Harris <teh019283@gmail.com>
2016-12-22 12:36:46 -0500

    now actually prints a christmas tree!

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,37 @@ use ctree::CTree;
 use term_painter::ToStyle;
 use term_painter::Color::*;
 use term_painter::Attr::*;
-use rand;
 use rand::Rng;
 
 fn main() {
     let t = CTree::new(10);
-    print_ctree(t);
+    print_ctree(&t);
 }
 
 fn print_ctree(t: &CTree) {
     let ocolors = vec![Red, Blue, Yellow];
+    let mut rng = rand::thread_rng();
 
-    for b in t.branches {
-        for i in 0..(b.width-t.max_width) {
+    for b in &t.branches {
+        for _ in 0..((t.max_width-b.width)/2) {
             print!(" ");
         }
 
         let mut os = b.ornaments.clone();
         os.reverse();
         let mut cur_ornament = os.pop();
-        for i in 0..width {
+
+        for i in 0..(b.width) {
             match (i, cur_ornament) {
-                (_, None) => print!("{}", Plain.bg(Green).paint(" "));
-                (i, Some(o)) where i == 0 => print
+                (x, Some(o)) if x == o => {
+                    let rc = ocolors[(rng.next_u32() as usize) % ocolors.len()];
+                    print!("{}", Plain.bg(rc).paint(" "));
+                    cur_ornament = os.pop();
+                },
+                _ => print!("{}", Plain.bg(Green).paint(" ")),
             }
         }
+
+        println!("");
     }
 }
